3541f3ae9111b39bf4f783d38e8eceb9b85fad82
Lily <helloimlily@gmail.com>
2022-04-07 09:20:10 +0900

    #1.2 Router Setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
